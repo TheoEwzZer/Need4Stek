40d4386a764083dab79c4a20ea627a7c32a66609
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-29 23:03:43 +0200

    🐛 fix: fix calculate_wheels_direction function to consider middle_left and middle_right lidar values 🐛 fix: fix calculate_speed function to consider middle_left and middle_right lidar values ✨ feat: add avoid_wall function to avoid walls when lidar detects an obstacle

--- src/control_car.c
+++ src/control_car.c
@@ -10,18 +10,18 @@
 float calculate_wheels_direction(car_t *car)
 {
     float wheels_dir_values[10] = {
-        0.45f, 0.4f, 0.35f, 0.3f, 0.25f, 0.20f, 0.15f, 0.10f, 0.05f, 0.0f
+        0.45f, 0.4f, 0.35f, 0.3f, 0.25f, 0.20f, 0.15f, 0.10f, 0.05f, 0.01f
     };
-    int distance[9] = {100, 200, 300, 400, 500, 700, 1020, 1350, 1700};
+    int distance[9] = {100, 200, 300, 400, 500, 600, 1020, 1350, 1700};
 
     for (unsigned int i = 0; car->right > car->left && i < 9; i++) {
-        if (car->middle < distance[i])
+        if (car->middle < distance[i] && car->middle_right < distance[i])
             return -wheels_dir_values[i];
     }
     if (car->right > car->left)
         return -wheels_dir_values[9];
     for (unsigned int i = 0; i < 9; i++) {
-        if (car->middle < distance[i])
+        if (car->middle < distance[i] && car->middle_left < distance[i])
             return wheels_dir_values[i];
     }
     return wheels_dir_values[9];
@@ -33,7 +33,9 @@ float calculate_speed(car_t *car)
     int speed_indicator_mid[8] = {2000, 1500, 1000, 800, 600, 400, 200, 100};
 
     for (unsigned int i = 0; i < 8; i++) {
-        if (car->middle > speed_indicator_mid[i])
+        if (car->middle > speed_indicator_mid[i]
+            && car->middle_left > speed_indicator_mid[i]
+            && car->middle_right > speed_indicator_mid[i])
             return speed[i];
     }
     return speed[8];
@@ -44,7 +46,7 @@ void check_end(car_t *car)
     bool can_stop = true;
 
     for (unsigned int i = 0; i < 32; i++) {
-        if (car->lidar[i] > 100.0f) {
+        if (car->lidar[i] > 90.0f) {
             can_stop = false;
             break;
         }
@@ -53,22 +55,43 @@ void check_end(car_t *car)
         end();
 }
 
+void avoid_wall(car_t *car)
+{
+    float wheels_dir = 0.0f;
+    char lineptr[100];
+
+    for (unsigned int i = 0; i < 32; i++) {
+        if (car->lidar[i] < 110.0f) {
+            write(STDOUT_FILENO, "CAR_FORWARD:0.00\n", 17);
+            fgets(lineptr, 100, stdin);
+            write(STDOUT_FILENO, "CAR_BACKWARDS:0.5f\n", 19);
+            fgets(lineptr, 100, stdin);
+            wheels_dir = (car->right > car->left) ? 0.5f : -0.5f;
+            dprintf(STDOUT_FILENO, "WHEELS_DIR:%.2f\n", (double)wheels_dir);
+            fgets(lineptr, 100, stdin);
+            return;
+        }
+    }
+}
+
 void control_car(car_t *car)
 {
-    char *lineptr = NULL;
-    float speed = calculate_speed(car);
-    float wheels_dir = calculate_wheels_direction(car);
-    size_t n = 0;
+    char lineptr[100];
+    float speed = 0.0f;
+    float wheels_dir = 0.0f;
 
+    avoid_wall(car);
+    speed = calculate_speed(car);
+    wheels_dir = calculate_wheels_direction(car);
     check_end(car);
     if (speed != car->current_speed) {
         dprintf(STDOUT_FILENO, "CAR_FORWARD:%.1f\n", (double)speed);
-        getline(&lineptr, &n, stdin);
+        fgets(lineptr, 100, stdin);
         car->current_speed = speed;
     }
     if (wheels_dir != car->current_wheels_direction) {
         dprintf(STDOUT_FILENO, "WHEELS_DIR:%.2f\n", (double)wheels_dir);
-        getline(&lineptr, &n, stdin);
+        fgets(lineptr, 100, stdin);
         car->current_wheels_direction = wheels_dir;
     }
 }
